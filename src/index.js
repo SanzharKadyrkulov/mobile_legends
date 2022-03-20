16ade8890773d0b4ee92ed604447c6d69b57a104
SanzharKadyrkulov <kadyrkulov.980@gmail.com>
2022-03-20 15:56:17 +0600

    delete alert

--- src/index.js
+++ src/index.js
@@ -3,9 +3,6 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import AuthContextProvider from './contexts/AuthContext';
-alert(`
-	Login: sancho@gmail.com
-	Password: 123456`);
 
 ReactDOM.render(
 	<AuthContextProvider>
